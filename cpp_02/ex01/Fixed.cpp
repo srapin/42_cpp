5bd97ef51b120b8c717d46ecc27f4a06bc9c46fa
Suzanne Rapin <suzanne.rapin@lilo.org>
2024-01-30 21:42:11 +0100

    cleaning

--- cpp_02/ex01/Fixed.cpp
+++ cpp_02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 01:00:22 by srapin            #+#    #+#             */
-/*   Updated: 2024/01/30 21:00:57 by srapin           ###   ########.fr       */
+/*   Updated: 2024/01/30 21:40:46 by srapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,48 +16,47 @@ int const       Fixed::_nFractionalBits = 8;
 
 Fixed::Fixed() : _fixedPoint(0)
 {
-    std::cout << "Default constructor called" << std::endl;
+//	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(Fixed const &fixed) : Fixed()
 {
-    std::cout << "Copy constructor called" << std::endl;
+//	std::cout << "Copy constructor called" << std::endl;
     setRawBits(fixed.getRawBits());
 }
 
 Fixed &Fixed::operator= (Fixed const &Fixed)
 {
-    std::cout << "Copy assignment operator called" << std::endl;
+//	std::cout << "Copy assignment operator called" << std::endl;
     setRawBits(Fixed.getRawBits());
     return (*this);
 } 
 
 int Fixed::getRawBits(void) const
 {
-    std::cout << "getRawBits member function called" << std::endl;
+    // std::cout << "getRawBits member function called" << std::endl;
     return _fixedPoint;
 }
 
 void Fixed::setRawBits(int const raw)
 {
-    std::cout << "setRawBits member function called" << std::endl;
+    // std::cout << "setRawBits member function called" << std::endl;
     _fixedPoint = raw;
 }
 
 Fixed::~Fixed()
 {
-    std::cout << "Destructor called" << std::endl;
+    //std::cout << "Destructor called" << std::endl;
 }
 
 Fixed::Fixed(int const i) {
 
-	std::cout << "Int constructor called" << std::endl;
+//	std::cout << "Int constructor called" << std::endl;
 	setRawBits(i * pow(2, _nFractionalBits));
 }
 
 Fixed::Fixed(float const f) {
-
-	std::cout << "Float constructor called" << std::endl;
+//	std::cout << "Float constructor called" << std::endl;
 	setRawBits(roundf(f * pow(2, _nFractionalBits)));
 }
 
--- cpp_02/ex03/bsp.cpp
+++ cpp_02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 02:42:06 by srapin            #+#    #+#             */
-/*   Updated: 2024/01/30 21:31:46 by srapin           ###   ########.fr       */
+/*   Updated: 2024/01/30 21:41:39 by srapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,5 @@ bool bsp( Point const a, Point const b, Point const c, Point const point)
     ab = foo(a,b, point);
     bc = foo(b, c, point);
     ac = foo(a, c, point);
-    std::cout << ab << " "<< bc << " " << ac << " " << ab +bc+ac << " " << obj << std::endl;
     return (ab + bc + ac == obj && ab && bc && ac);
 }
