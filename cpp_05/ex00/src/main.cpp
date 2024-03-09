51617419c21bc660e875e0a9d4e36286516b9492
srapin <suzanne.rapin@ulb.be>
2024-03-09 16:29:42 +0100

    better_main

--- cpp_05/ex00/src/main.cpp
+++ cpp_05/ex00/src/main.cpp
@@ -6,70 +6,18 @@
 /*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 18:10:10 by srapin            #+#    #+#             */
-/*   Updated: 2024/03/05 16:44:32 by srapin           ###   ########.fr       */
+/*   Updated: 2024/03/09 16:29:20 by srapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Bureaucrat.hpp"
 
-int main(void)
+void invalideBureaucrateCreation()
 {
-	Bureaucrat b1("Bob", 1);
-	Bureaucrat b2("Joe", 150);
-	
-	std::cout << "\n" << b1;
-	std::cout << b2 << std::endl;
-
-	//testing invalide incrementation 
-	try
-	{
-		++b1;
-	}
-	catch (Bureaucrat::GradeTooHighException &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	
-	//testing invalide decrementation 
-	
-	try
-	{
-		--b2;
-	}
-	catch(Bureaucrat::GradeTooLowException &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	
-	std::cout << "\n" << b1;
-	std::cout << b2 << std::endl;
-	
-	//testing valide incrementation 
-	try
-	{
-		--b1;
-	}
-	catch (Bureaucrat::GradeTooLowException &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	//testing valide decrementation 
-	try
-	{
-		++b2;
-	}
-	catch (Bureaucrat::GradeTooHighException &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-
-	std::cout << "\n" << b1;
-	std::cout << b2 << std::endl;
-	
-	//testing invalide creation 
 	try
 	{
 		Bureaucrat bErr("Bob", 0);
+		std::cout << "------------------UNEXPECTED--------------------------" << std::endl;
 	}
 	catch (Bureaucrat::GradeTooHighException &e)
 	{
@@ -78,9 +26,63 @@ int main(void)
 	try
 	{
 		Bureaucrat bErr("Bob", 151);
+		std::cout << "------------------UNEXPECTED--------------------------" << std::endl;
 	}
 	catch (Bureaucrat::GradeTooLowException &e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
+}
+
+void invalideDecIncOnBureaucrate()
+{
+	Bureaucrat b1("Bob", 1);
+	
+	std::cout << b1;
+	//testing invalide incrementation 
+	try
+	{
+		++b1;
+		std::cout << "------------------UNEXPECTED--------------------------" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooHighException &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	//testing invalide decrementation 
+	Bureaucrat b2("Joe", 150);
+	std::cout << b2 ;
+	
+	try
+	{
+		--b2;
+		std::cout << "------------------UNEXPECTED--------------------------" << std::endl;
+	}
+	catch(Bureaucrat::GradeTooLowException &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+void valideDecIncOnBureaucrate()
+{
+	Bureaucrat b1("Bob", 75);
+	
+	std::cout << b1;
+	++b1;
+	if (b1.getGrade() != 74)
+		std::cout << "------------------UNEXPECTED--------------------------" << std::endl;
+	std::cout << b1;
+	--b1;
+	if (b1.getGrade() != 75)
+		std::cout << "------------------UNEXPECTED--------------------------" << std::endl;
+	std::cout << b1;
+}
+
+int main(void)
+{
+	invalideBureaucrateCreation();
+	invalideDecIncOnBureaucrate();
+	valideDecIncOnBureaucrate();
 }
